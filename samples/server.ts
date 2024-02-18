429411e09bce83d0f00db0c87bbc2212f0f134d4
skytomo <tomo1kawa.fullerene@gmail.com>
2024-02-18 10:50:46 +0900

    feat: #1 remove invalid event listeners

--- samples/server.ts
+++ samples/server.ts
@@ -2,10 +2,6 @@ import OtamashelfServer from '../src/OtamashelfServer';
 
 const server = new OtamashelfServer({ port: 3000 });
 server.on('error', console.error);
-server.on('open', function open() {
-  console.log('connected');
-});
-console.log('start');
 server.on('connection', function connection(ws) {
   console.log('connected');
   ws.on('message', message => console.log(message.toString()));
